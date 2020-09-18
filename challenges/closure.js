1f32f5de70cffe2c94f1271862bafd3c21288032
gerardo-rivera3901 <gerardo.rivera3901@gmail.com>
2020-09-18 10:19:23 -0700

    Completed all tasks in JS files

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,18 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: The const "internal" and the nestedFunction are both inside of the myFunction block so the nestedFunction is able to call upon anything withing that scope. It can reach back but not forward into other functions (if there were any others).
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(parameter){
+  let total = 0
+  for(var i = 0; i <= parameter; i++){
+    total += i
+    console.log(total)
+  }
+}
+summation(4)
